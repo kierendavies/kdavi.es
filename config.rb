c345a0c3a6af4702f22ebe64f0334b3ffbc4dced
Kieren Davies <kieren@kdavi.es>
2015-12-31 17:17:15 +0200

    Support IPFS

--- config.rb
+++ config.rb
@@ -47,6 +47,8 @@
 #   end
 # end
 
+set :relative_links, true
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -68,7 +70,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
